0c152165b5fef7a62ed19ffda52a4598833904ab
Virtuos86 <virtuos86@yandex.ru>
2021-12-11 06:32:40 +0700

    .

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,7 @@ mod texts;
 const DEBUG: bool = true;
 
 fn main() -> Result<(), cursive::theme::Error> {
+    use cursive::CursiveExt;
     let mut app: cursive::Cursive = cursive::Cursive::default();
     app.load_theme_file("assets/theme.toml")?;
     low_level::VideoInitialize();
